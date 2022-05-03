ccc44ce1e12a120a5b0366595b46cc0a7fcdd5fe
Erick Falconi <raidenjr28@gmail.com>
2022-05-02 21:36:16 -0400

    Adding styles to REST-Rant

--- views/default.jsx
+++ views/default.jsx
@@ -4,6 +4,8 @@ return (
     <html>
         <head>
             <title>Title</title>
+            <link rel="stylsheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"></link>
+            <link rel="stylesheet" href="/css/style.css"></link>
         </head>
         <body>
             {html.children}
