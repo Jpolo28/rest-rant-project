378aca6bfeee5ad1e91e5152d9334ad696f7d2ad
Erick Falconi <raidenjr28@gmail.com>
2022-05-04 19:32:43 -0400

    Navbar with style

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,11 @@ function home () {
     <Def>
         <main>
             <h1>HOME</h1>
+            <div><img src="/images/burger.jpg" alt="Burger"></img>
+            <div>
+              Photo by <a href="https://unsplash.com/@1ncreased">Lidye</a> on<a href="unsplash.com"></a>
+            </div>
+            </div>
             <a href="/places">
               <button className="btn-primary">Places Page</button>
             </a>
