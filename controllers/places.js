6196ccd39e8ae7c13b62e4b7e815c17c1e44e1df
Erick Falconi <raidenjr28@gmail.com>
2022-05-09 14:33:52 -0400

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -26,17 +26,17 @@ router.get('/:id', (req, res) => {
 router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-      res.render('error404')
+    res.render('error404')
   }
   else if (!places[id]) {
     res.render(error404)
   }
   else {
-    res.render('places/edit', { place: places[id]})
+    res.render('places/edit', { place: places[id] })
   }
-  
-})
 
+})
+// POST
 router.post('/', (req, res) => {
   if (!req.body.pic) {
     // Default image if one is not provided
@@ -50,6 +50,33 @@ router.post('/', (req, res) => {
   }
   places.push(req.body)
   res.redirect('/places')
+})
+
+// PUT
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    // Dif into req.body and make sure data is valid
+    if (!req.body.pic) {
+      req.body.pic = "http://placekitten.com/400/400"
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+
+    // Save the new data into places[id]
+    places[id] = req.body
+    res.redirect(`/places/${id}`)
+  }
 })
 
 
