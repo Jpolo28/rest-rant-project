499ea8b8294ef7acff3be75f00b8ec3de31f706a
Erick Falconi <raidenjr28@gmail.com>
2022-05-16 18:52:22 -0400

    Rest-Rant-Part-9-Progress-#9step1

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 // Modules and Globals
 require('dotenv').config()
-const express = require ('express')
+const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
+const mongoose = require('mongoose')
+
 
 // Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 //Controllers & Routes
-app.use ('/places', require('./controllers/places'))
+app.use('/places', require('./controllers/places'))
 
-app.get ('/', (req, res) => {
+app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get ('*', (req,res) => {
+app.get('*', (req, res) => {
     res.render('error404')
 })
 
-app.listen (process.env.PORT)
+app.listen(process.env.PORT)
