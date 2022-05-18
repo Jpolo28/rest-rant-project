b78d46980962550a3d2c3768f161fbbd58535890
Erick Falconi <raidenjr28@gmail.com>
2022-05-18 14:44:02 -0400

    finished-rest-rant-part-9

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,18 +5,35 @@ function show(data) {
     return (
         <Def>
             <main>
-                <h1>{data.place.name}</h1>
-                <h2>Rating</h2>
-                <h2>Description</h2>
-                <h2>Comments</h2>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                    Edit
-                </a>
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> <button type="submit" className='btn btn-danger'>
-                    Delete
-                </button>
+                <div className='row'>
+                    <div className='col-sm-6'>
+                        <img src={data.place.city} alt={data.place.name} />
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
 
-                </form>
+                    <div className='col-sm-6'>
+                        <h1>{data.place.name}</h1>
+                        <h2>Rating</h2>
+                        <h2>Description</h2>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.places.cuisines}
+                        </h4>
+                        <h2>Comments</h2>
+                        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                            Edit
+                        </a>
+                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}> <button type="submit" className='btn btn-danger'>
+                            Delete
+                        </button>
+                        </form>
+                    </div>
+
+                </div>
             </main>
         </Def>
     )
